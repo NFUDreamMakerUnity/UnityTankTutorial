2d4b621139b7d25d3e79d2560dfd4d429eb502bb
吳孟紘 <wueric0622@gmail.com>
2017-10-03 19:53:37 +0800

    初始版本

---
+++ Assets/AIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour {
+    GameObject Player;
+    Vector3 CurrentPathPosition;
+    TankMovementController tankMovementController;
+
+    // Use this for initialization
+    void Start () {
+        tankMovementController = GetComponent<TankMovementController>();
+        Player = FindObjectOfType<PlayerController>().gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 Direction = (Player.transform.position - transform.position).normalized;
+        tankMovementController.MoveForward(Vector3.Dot(transform.position.normalized,Direction));
+		tankMovementController.Turn(Vector3.Cross(transform.position.normalized,Direction).x*10);
+
+        //Debug.DrawLine(NavMeshAgent.nextPosition, NavMeshAgent.nextPosition.normalized * 1, Color.red);
+		Debug.DrawLine(transform.position, Direction, Color.blue);
+
+        print(Vector3.Cross(transform.position.normalized,Direction).x);
+
+    }
+}
---
+++ Assets/TankMovementController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovementController : MonoBehaviour {
+
+	[SerializeField]
+    float Power = 1000;
+    [SerializeField]
+    GameObject WheelL;
+    [SerializeField]
+    GameObject WheelR;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.DrawLine(WheelL.transform.position, WheelL.transform.forward * Power, Color.blue);
+
+    }
+    public void MoveForward(float forward)
+    {
+        GetComponent<Rigidbody>().AddForceAtPosition(forward * WheelL.transform.forward * Power, WheelL.transform.position);
+        GetComponent<Rigidbody>().AddForceAtPosition(forward * WheelR.transform.forward * Power, WheelR.transform.position);
+        
+    }
+
+        public void Turn(float Turn)
+    {
+        GetComponent<Rigidbody>().AddForceAtPosition(-Turn * WheelL.transform.forward * Power, WheelL.transform.position);
+        GetComponent<Rigidbody>().AddForceAtPosition(Turn * WheelR.transform.forward * Power, WheelR.transform.position);
+        
+    }
+}
